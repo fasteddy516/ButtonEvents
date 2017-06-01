3aec65db9ec829ea0fe5f1358a19a1c279ce4609
Edward Wright <fasteddy@thewrightspace.net>
2017-06-01 00:09:12 -0400

    added actual library code

--- src/ButtonEvents.cpp
+++ src/ButtonEvents.cpp
@@ -1,52 +1,162 @@
-/*
-  Test.h - Test library for Wiring - implementation
-  Copyright (c) 2006 John Doe.  All right reserved.
-*/
+#include "Arduino.h"
+#include "ButtonEvents.h"
 
-// include core Wiring API
-#include "WProgram.h"
-
-// include this library's description file
-#include "Test.h"
-
-// include description files for other libraries used (if any)
-#include "HardwareSerial.h"
-
-// Constructor /////////////////////////////////////////////////////////////////
-// Function that handles the creation and setup of instances
-
-Test::Test(int givenValue)
-{
-  // initialize this instance's variables
-  value = givenValue;
-
-  // do whatever is required to initialize the library
-  pinMode(13, OUTPUT);
-  Serial.begin(9600);
+// default (and only) constructor
+ButtonEvents::ButtonEvents() {
+  debouncedButton.interval(DEFAULT_DEBOUNCE_MS);
+  doubleTapTime_ms = DEFAULT_DOUBLETAP_MS;
+	holdTime_ms = DEFAULT_HOLD_MS;
+  isActiveLow = DEFAULT_ACTIVE_LOW;
+	pressTime_ms = 0; // initialize button timestamps and states...
+	releaseTime_ms = 0;
+	buttonState = idle;
+	buttonEvent = none;
 }
 
-// Public Methods //////////////////////////////////////////////////////////////
-// Functions available in Wiring sketches, this library, and other libraries
-
-void Test::doSomething(void)
-{
-  // eventhough this function is public, it can access
-  // and modify this library's private variables
-  Serial.print("value is ");
-  Serial.println(value);
-
-  // it can also call private functions of this library
-  doSomethingSecret();
+// passthru to Bounce2 attach() method
+void ButtonEvents::attach(int pin) {
+	debouncedButton.attach(pin);
 }
 
-// Private Methods /////////////////////////////////////////////////////////////
-// Functions only available to other functions in this library
-
-void Test::doSomethingSecret(void)
-{
-  digitalWrite(13, HIGH);
-  delay(200);
-  digitalWrite(13, LOW);
-  delay(200);
+// passthru to Bounce2 attach() overload
+void ButtonEvents::attach(int pin, int mode) {
+	debouncedButton.attach(pin, mode);
 }
 
+// set button mode to active high
+void ButtonEvents::activeHigh() {
+  isActiveLow = false;
+}
+
+// set button mode to active low
+void ButtonEvents::activeLow() {
+  isActiveLow = true;
+}
+
+// alias/passthru to Bounce2 interval() method
+void ButtonEvents::debounceTime(unsigned int debounce_ms) {
+  debouncedButton.interval(debounce_ms);
+}
+
+// method to set the 'doubleTap' event detection window
+void ButtonEvents::doubleTapTime(unsigned int doubleTap_ms) {
+  doubleTapTime_ms = doubleTap_ms;
+}
+
+// method to set the amount of time that must elapse to trigger a 'hold' event
+void ButtonEvents::holdTime(unsigned int hold_ms) {
+  holdTime_ms = hold_ms;
+}
+
+// passthru to Bounce2 interval() method
+void ButtonEvents::interval(unsigned int interval_ms) {
+  debouncedButton.interval(interval_ms);
+}
+
+// returns true if button was pressed (accounts for active high/low)
+bool ButtonEvents::buttonPressed() {
+  if (isActiveLow && debouncedButton.fell()) return true;
+  else if (!isActiveLow && debouncedButton.rose()) return true;
+  return false;
+}
+
+// returns true if button was released (accounts for active high/low)
+bool ButtonEvents::buttonReleased() {
+  if (isActiveLow && debouncedButton.rose()) return true;
+  else if (!isActiveLow && debouncedButton.fell()) return true;
+  return false;
+}
+
+// calls the Bounce2 update() method, then runs button event detection logic
+bool ButtonEvents::update() {
+	bool passthruState = debouncedButton.update(); // update debounced button state
+
+	if (buttonPressed()) {
+
+		// if the button was previously idle, store the press time and update the button state
+		if (buttonState == idle) {
+			pressTime_ms = millis();
+			buttonState = pressed;
+		}
+
+		// if the button was in a released state (waiting for a double tap), update the button
+		// state and indicate that a double tap event occurred
+		else if (buttonState == released) {
+			buttonState = idle;
+			buttonEvent = doubleTap;
+			return true;
+		}
+	}
+
+	else if (buttonReleased()) {
+
+		// if the button was in a pressed state, store the release time and update the button state
+		if (buttonState == pressed) {
+			releaseTime_ms = millis();
+			buttonState = released;
+		}
+	}
+
+	// if the button is currently in a pressed state...
+	if (buttonState == pressed) {
+
+		// if the specified hold time has been reached or passed, update the button state and
+		// indicate that a hold event occurred
+		if ((millis() - pressTime_ms) >= holdTime_ms) {
+			buttonState = idle;
+			buttonEvent = hold;
+			return true;
+		}
+	}
+
+	// if the button is currently in a released state...
+	else if (buttonState == released) {
+
+		// if the specified double tap time has been reached or passed, update the button state
+		// and indicate that a (single) tap event occurred
+		if ((millis() - releaseTime_ms) >= doubleTapTime_ms) {
+			buttonState = idle;
+			buttonEvent = tap;
+			return true;
+		}
+	}
+
+	// if we get to this point, indicate that no button event occurred in this cycle
+	buttonEvent = none;
+	return passthruState;
+}
+
+// returns the last triggered event
+ButtonEvent ButtonEvents::event() {
+	return buttonEvent;
+}
+
+// returns true if button was tapped
+bool ButtonEvents::tapped() {
+  return (buttonEvent == tap);
+}
+
+// returns true if button was double-tapped
+bool ButtonEvents::doubleTapped() {
+  return (buttonEvent == doubleTap);
+}
+
+// returns true if button was held
+bool ButtonEvents::held() {
+  return (buttonEvent == hold);
+}
+
+// passthru to Bounce2 read() method
+bool ButtonEvents::read() {
+	return debouncedButton.read();
+}
+
+// passthru to Bounce2 fell() method
+bool ButtonEvents::fell() {
+	return debouncedButton.fell();
+}
+
+// passthru to Bounce2 rose() method
+bool ButtonEvents::rose() {
+	return debouncedButton.rose();
+}
--- src/ButtonEvents.h
+++ src/ButtonEvents.h
@@ -1,27 +1,79 @@
 /*
-  Test.h - Test library for Wiring - description
-  Copyright (c) 2006 John Doe.  All right reserved.
+MIT License
+
+Copyright (c) 2017 fasteddy516
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 */
 
-// ensure this library description is only included once
-#ifndef Test_h
-#define Test_h
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * *
+Written by Edward Wright (fasteddy@thewrightspace.net)
+Utilizes the Bounce2 library (https://github.com/thomasfredericks/Bounce2) by Thomas O Fredericks (tof@t-o-f.info)
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
-// include types & constants of Wiring core API
-#include "WConstants.h"
+#ifndef ButtonEvents_h
+#define ButtonEvents_h
 
-// library interface description
-class Test
-{
-  // user-accessible "public" interface
+#include <Bounce2.h> // use Thomas Fredericks' button debounce library
+
+// these are the default settings used in the constructor - all of them can be changed after instantiation using the corresponding methods 
+#define DEFAULT_DEBOUNCE_MS   35
+#define DEFAULT_DOUBLETAP_MS  150
+#define DEFAULT_HOLD_MS       500
+#define DEFAULT_ACTIVE_LOW    true
+
+// enumerations to keep things readable
+enum ButtonState { idle, pressed, released };
+enum ButtonEvent { none, tap, doubleTap, hold };
+
+class ButtonEvents {
   public:
-    Test(int);
-    void doSomething(void);
+		ButtonEvents(); // default constructor - follows the argument-free Bounce2 convention.  Use defaults, or set explicitly later
+		void attach(int pin); // passthru to Bounce2 attach() method
+		void attach(int pin, int mode); // passthru to Bounce2 attach() overload
+    void activeHigh(); // set button mode to active high
+    void activeLow(); // set button mode to active low
+    void debounceTime(unsigned int debounce_ms); // alias/passthru to Bounce2 interval() method
+    void doubleTapTime(unsigned int doubleTap_ms); // method for setting the doubleTap event detection window
+    void holdTime(unsigned int hold_ms); // method for setting the time required to trigger a hold event
+    void interval(unsigned int interval_ms); // passthru to Bounce2 interval() method
+		bool update(); // calls the Bounce2 update() method, then runs button event detection logic
+		ButtonEvent event(); // returns the button event detected during update() call
+    bool tapped(); // returns true if the 'tap' event was detected 
+    bool doubleTapped(); // returns true if the 'doubleTap' event was detected
+    bool held(); // returns true if the 'held' event was detected
+    bool read(); // passthru to Bounce2 read() method;
+		bool fell(); // passthru to Bounce2 fell() method;
+		bool rose(); // passthru to Bounce2 rose() method;
+		
+	private:
+		unsigned long pressTime_ms; // remember when the button was pressed
+		unsigned long releaseTime_ms; // remember when the button was released
+		unsigned int doubleTapTime_ms; // how long to wait for a double tap after the initial button release 
+		unsigned int holdTime_ms; // how long the button must be held to generate a hold event
+		ButtonState buttonState; // current button state
+		ButtonEvent buttonEvent; // detected button event
+    bool isActiveLow;
+		Bounce debouncedButton; // button debounced using Thomas Fredericks' Bounce2 library
 
-  // library-accessible "private" interface
-  private:
-    int value;
-    void doSomethingSecret(void);
+    bool buttonPressed(); // returns true if the button was pressed (accounts for active high/low)
+    bool buttonReleased(); // returns true if the button was released (accounts for active high/low)
 };
 
 #endif
